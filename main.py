18d258aa4867b1fcc1a4bf63b33f023d7f4e2a35
shelly-serafimovich <81259820+shelly-serafimovich@users.noreply.github.com>
2024-04-09 22:30:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 if __name__ == '__main__':
     post_df = pd.read_csv("posts_res.csv")
-    post_df = preprocess(post_df)
+    post_df = preprocess(post_df, train_flag=True)
     model_name = "Logistic Regression"
     train(post_df, model_name)
